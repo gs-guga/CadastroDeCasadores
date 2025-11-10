227c962b0a2b881150efb13def193899622543b4
gs-guga <gustavocarvalho933085@gmail.com>
2025-11-10 15:34:04 -0300

    primeiro commit

---
+++ src/main/java/dev/java10x/CadastroDeCasadores/CadastroDeCasadoresApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeCasadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeCasadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeCasadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeCasadores/CadastroDeCasadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeCasadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeCasadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
